e664e6ba925828123ae9590b8ff59b44104ac402
Arba Maxhuni <am67634@ubt-uni.net>
2025-01-16 22:33:33 +0100

    pjesa e cart-it

--- arbys/Connection.cs
+++ arbys/Connection.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
@@ -17,6 +20,8 @@ namespace arbys
 
     public class Utils
     {
+        SqlConnection con;
+        SqlCommand cmd;
         public static bool IsValidExtension(string fileName)
         {
             bool isValid = false;
@@ -46,6 +51,34 @@ namespace arbys
             }
             //return ResolveUrl(url1);
             return url1;
+        }
+
+        public bool updateCartQuantity(int quantity, int productId, int userId)
+        {
+            bool isUpdated = false;
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Cart_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", "UPDATE");
+            cmd.Parameters.AddWithValue("@ProductId", productId);
+            cmd.Parameters.AddWithValue("@Quantity", quantity);
+            cmd.Parameters.AddWithValue("@UserId", userId);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                isUpdated = true;
+            }
+            catch (Exception ex)
+            {
+                isUpdated = false;
+                System.Web.HttpContext.Current.Response.Write("<script>alert('Error - " + ex.Message + " ');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+            return isUpdated;
         }
     }
 }
--- arbys/User/Menu.aspx
+++ arbys/User/Menu.aspx
@@ -11,6 +11,9 @@
     <section class="food_section layout_padding">
         <div class="container">
             <div class="heading_container heading_center">
+                <div class="align-self-end">
+                    <asp:Label ID="lblMsg" runat="server" Visible="false"></asp:Label>
+                </div>
                 <h2>Our Menu
                 </h2>
             </div>
@@ -27,7 +30,7 @@
 
             <div class="filters-content">
                 <div class="row grid">
-                    <asp:Repeater ID="rProducts" runat="server">
+                    <asp:Repeater ID="rProducts" runat="server" OnItemCommand="rProducts_ItemCommand">
                         <ItemTemplate>
                             <div class="col-sm-6 col-lg-4 all <%# Regex.Replace(Eval("CategoryName").ToString().ToLower(),@"\s+","") %>">
                                 <div class="box">
